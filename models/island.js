9af672c86be14d3158b241fd52eb4093041678e6
Spencer <sr23513@gmail.com>
2019-04-29 22:38:47 +0000

    first commit

---
+++ models/island.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+var islandSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+
+module.exports = mongoose.model("Island", islandSchema);
